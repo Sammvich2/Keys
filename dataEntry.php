92aec1aff9092402dc7f4d663317a73595918362
Sam <sammvich@gmail.com>
2021-08-16 20:21:10 +1000

    / ‘dataEntry.php’

--- dataEntry.php
+++ dataEntry.php
@@ -64,7 +64,7 @@
         <td><h2>When?</h2></td>
         <td><h2>Issuer</h2></td>
         <td><h2>Large?</h2></td>
-        <td><h2>FIP</h2></td>
+        <td><h2> FIP </h2></td>
         <td><h2>Pump</h2></td>
         <td><h2>Access Details</h2></td>
         <td><h2>Key?</h2></td>
