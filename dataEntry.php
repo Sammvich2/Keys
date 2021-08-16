8d65a77aaf752536edbcc5a633eaf96fe1a07f21
Sam <sammvich@gmail.com>
2021-08-16 18:39:26 +1000

    / ‘dataEntry.php’

--- dataEntry.php
+++ dataEntry.php
@@ -1,1 +1,51 @@
+<html>
+<head>
+    <title>Da keys</title>
+    <style>
+        body {
+            font-family: Century Gothic, CenturyGothic, AppleGothic, sans-serif;
+            background: #1f1f1f;
+            color: #fafafa;
+        }
+
+        table, tr, td {
+            border: 2px solid black;
+            border-collapse: collapse;
+
+        }
+        table {
+            width: 75%;
+            margin: -100px auto;
+        }
+
+        tr {
+            height: 30px;
+            font-size: 23px;
+        }
+
+        td {
+            font-size: 28px;
+        }
+
+        h1 {
+            font-size: 300px;
+            text-align: center;
+            color: #ff0033
+        }
+        h2 {
+            font-size: 30px;
+        }
+
+        h3 {
+            font-size: 20px;
+        }
+    </style>
+</head>
+<body>
+<h1>
+    Da Keys
+</h1>
+
+
 <?php
+
