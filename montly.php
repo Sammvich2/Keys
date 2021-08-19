967afbbc9a61803566406dc04e40cca59130063e
Sam <sammvich@gmail.com>
2021-08-19 18:57:29 +1000

    / ‘montly.php’

--- montly.php
+++ montly.php
@@ -1,1 +1,90 @@
+<html>
+<html>
+<head>
+    <title>Key Entry</title>
+    <style>
+        body {
+            font-family: Century Gothic, CenturyGothic, AppleGothic, sans-serif;
+            background: #1f1f1f;
+            color: #fafafa;
+        }
+
+        table, tr, td {
+            border: none;
+            border-collapse: collapse;
+
+        }
+        tr:nth-child(even) {
+            background-color: #5c5c5c;
+        }
+
+        table {
+            width: 90%;
+            margin: 100px auto;
+            text-align: center;
+        }
+
+        tr {
+            height: 30px;
+            font-size: 23px;
+        }
+
+        td {
+            font-size: 28px;
+        }
+
+        td.fip {
+            width: 75px;
+        }
+
+        h1 {
+            font-size: 100px;
+            text-align: center;
+            color: #ff0033
+        }
+        h2 {
+            font-size: 30px;
+        }
+
+        h3 {
+            font-size: 20px;
+        }
+        input {
+            font-size: 30px;
+            resize: none;
+            font-family: Century Gothic, CenturyGothic, AppleGothic, sans-serif;
+            width: 100%;
+
+        }
+
+        input.check {
+            height: 85%;
+        }
+    </style>
+</head>
+
+<h1>
+    Montlies
+</h1>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</html>
+
+
+
+
 <?php
