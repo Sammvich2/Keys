6754000c0a8ba93f5d1c6615d548afdf763d65eb
Sam <sammvich@gmail.com>
2021-08-19 20:39:40 +1000

    / ‘monthly.php’

--- monthly.php
+++ monthly.php
@@ -76,7 +76,7 @@
 
     <tr>
 
-        <td><h4><input style="width: %" type="submit" value="Hide Yes" name="hide"></h4></td>
+        <td><h4><input formaction="monthly.php" formmethod="post"  type="submit" value="Hide Yes" name="hide"></h4></td>
 
     </tr>
 
