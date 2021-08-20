82256cc2e5c858de3744903ab367fa3937243a21
Sam <sammvich@gmail.com>
2021-08-20 17:36:10 +1000

    / ‘monthly.php’

--- monthly.php
+++ monthly.php
@@ -76,8 +76,20 @@
 
 <table>
     <tr>
-        <td style="background-color: #1f1f1f"><h4><form action="monthly.php" method="post"><input type="submit" value="Hide Yes" name="hide"></form></h4></td>
-        <td style="background-color: #1f1f1f"><h4><button formaction="site_lookup.php" type="button">Go To Sitelookup</button></h4></td>
+        <td style="background-color: #1f1f1f">
+            <h4>
+                <form action="monthly.php" method="post">
+                    <input type="submit" value="Hide Yes" name="hide">
+                </form>
+            </h4>
+        </td>
+        <td style="background-color: #1f1f1f">
+            <h4>
+                <form action="site_lookup.php">
+                    <button formaction="site_lookup.php" type="button">Go To Sitelookup</button>
+                </form>
+            </h4>
+        </td>
     </tr>
 
     <tr>
