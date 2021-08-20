c012c93e919246484a1cbfb51deb6f4c9d9d10b0
Sam <sammvich@gmail.com>
2021-08-20 21:13:43 +1000

    / ‘monthly.php’

--- monthly.php
+++ monthly.php
@@ -129,14 +129,12 @@
         foreach($keys as $row => $key){
             echo "<tr><h3>";
             echo "<td style='padding-top: 1%; padding-bottom: 1%'>" .  $key['fip']  .  "</td>";
-            echo "<td style='padding-top: 1%'><form action='monthly.php' method='post'><input  type='submit' name='done' value=" . $key['fip'] . "></form></td>";
-
+            echo "<td style='padding-top: 1%'><form action='monthly.php' method='post'><input  type='submit' name='done' value=" . $key['address'] . "></form></td>";
             echo "<td style='text-align: left; padding-left: 5%'>" .  $key['address']  .  "</td>";
             echo "<td style='text-align: left; padding-left: 5%'>" .  $key['access']  .  "</td>";
             echo "<td>" .  $key['is_key']  .  "</td>";
 
 
-            echo "<td style='padding-top: 1%'><form action='monthly.php' method='post'><input  type='submit' name='done' value=" . $key['unique'] . "></form></td>";
 
 
             echo "</h3></tr>";
