e32ce28bdf0885fce36d89c833930717073c5443
Sam <sammvich@gmail.com>
2021-08-20 17:49:57 +1000

    / ‘monthly.php’

--- monthly.php
+++ monthly.php
@@ -85,7 +85,7 @@
         </td>
         <td style="background-color: #1f1f1f">
             <h4>
-                <button id="myButton"  ><h4>Go To Site Lookup</h4></button>
+                <button id="myButton"  >Go To Site Lookup</button>
 
                 <script type="text/javascript">
                     document.getElementById("myButton").onclick = function () {
