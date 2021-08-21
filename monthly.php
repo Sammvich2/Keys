3f8064d4899de5ebbe828bfb354bc6bf7bfd796d
Sam <sammvich@gmail.com>
2021-08-21 13:01:37 +1000

    / ‘monthly.php’

--- monthly.php
+++ monthly.php
@@ -143,13 +143,13 @@
             $_POST == null;
             echo "<script> setTimeout(function() {
                 window.location.href = window.location.pathname;
-            }, 1000);</script>";
+            }, 500);</script>";
         } elseif ($done['fip'] == "No") {
             $change = $pdo->query("UPDATE keys SET fip = 'Yes' WHERE id_number IS " . $_POST['done']);
             $_POST == null;
             echo "<script> setTimeout(function() {
                 window.location.href = window.location.pathname
-            }, 1000);</script>";
+            }, 500);</script>";
         } else {
             print_r("Update Failed!");
         }
