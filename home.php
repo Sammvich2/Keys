22ee89046346fb7a6ca628b91d4f201f80460d2e
Sam <sammvich@gmail.com>
2021-08-21 21:23:01 +1000

    / ‘home.php’

--- home.php
+++ home.php
@@ -1,1 +1,64 @@
+<html>
+    <head>
+        <title>Login</title>
+
+        <style>
+            body {
+            font-family: Century Gothic, CenturyGothic, AppleGothic, sans-serif;
+            background: #1f1f1f;
+            color: #fafafa;
+            }
+
+            table, tr, td {
+            border: none;
+            border-collapse: collapse;
+
+            }
+
+            tr:nth-child(even) {
+            background-color: #454545;
+            }
+
+            table {
+            width: 100%;
+            margin: 100px auto;
+            }
+
+            tr {
+            height: 30px;
+            }
+
+            td {
+            font-size: 28px;
+            }
+
+            h1 {
+            font-size: 150px;
+            text-align: center;
+            color: #ff0033
+            }
+            h2 {
+            font-size: 45px;
+            }
+
+            h3 {
+            font-size: 45px;
+            }
+        </style>
+
+    </head>
+
+    <body>
+        <h1>Login</h1>
+    
+
+
+    </body>
+
+
+
+
+</html>
+
+
 <?php
