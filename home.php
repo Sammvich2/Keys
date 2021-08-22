b8f2b7a4ad0445fdb863f3b635c828c63cea47fc
Sam <sammvich@gmail.com>
2021-08-22 12:20:18 +1000

    / ‘home.php’

--- home.php
+++ home.php
@@ -87,8 +87,8 @@ error_reporting(E_ALL);
 
         $pdo = new PDO('sqlite:users.db');
         $pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-        $inputUser = $_POST['user'];
-        $userQ = $pdo->query('SELECT * from users WHERE username IS "'. $inputUser . '"');
+        $inputUser = "sam";
+        $userQ = $pdo->query("SELECT * from users WHERE username IS '". $inputUser . "'");
         print_r("SELECT * from users WHERE username IS '" . $inputUser . "'");
 
         $users = $userQ->fetchAll(PDO::FETCH_ASSOC);
