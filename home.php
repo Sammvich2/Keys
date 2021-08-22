1738ca3d9d31db72cdb191c47219342fa69e0a14
Sam <sammvich@gmail.com>
2021-08-22 12:30:20 +1000

    / ‘home.php’

--- home.php
+++ home.php
@@ -88,7 +88,7 @@ error_reporting(E_ALL);
         $pdo = new PDO('sqlite:keys.db');
         $pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
         $inputUser = "Same";
-        $userQ = $pdo->query("SELECT * from users WHERE username IS '". $inputUser . "'");
+        $userQ = $pdo->query("SELECT * from people WHERE account IS '". $inputUser . "'");
         print_r("SELECT * from users WHERE unique IS " . $inputUser);
 
         $users = $userQ->fetchAll(PDO::FETCH_ASSOC);
