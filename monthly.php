4502c5b70f52929e9b8195f9724de4b4050b9223
Sam <sammvich@gmail.com>
2021-08-22 17:02:46 +1000

    / ‘.idea/dataSources.local.xml’ / ‘monthly.php’

--- monthly.php
+++ monthly.php
@@ -104,9 +104,9 @@
 
 
     <?php
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
+    #ini_set('display_errors', 1);
+    #ini_set('display_startup_errors', 1);
+    #error_reporting(E_ALL);
 
     if(!isset($_COOKIE['sessionID'])) {
         print_r("Wire function to return to login");
