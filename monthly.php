36863e96ad38f0a2e9237d1b10ea88f156a3337a
Sam <sammvich@gmail.com>
2021-08-22 17:00:59 +1000

    / ‘monthly.php’

--- monthly.php
+++ monthly.php
@@ -152,6 +152,8 @@
 
             if ($done['fip'] == "Yes") {
                 $change = $pdo->query("UPDATE keys SET fip = 'No' WHERE id_number IS " . $_POST['done']);
+                $change = $pdo->query("UPDATE keys SET monthly = NULL WHERE id_number IS " . $_POST['done']);
+
                 $_POST == null;
                 echo "<script> setTimeout(function() {
                 window.location.href = window.location.pathname;
