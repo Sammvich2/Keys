a4de45b8261fdf5c01619c67d9d57b3d13d17662
Sam <sammvich@gmail.com>
2021-08-22 16:43:41 +1000

    / ‘monthly.php’

--- monthly.php
+++ monthly.php
@@ -104,6 +104,10 @@
 
 
     <?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
     if(!isset($_COOKIE['sessionID'])) {
         print_r("Wire function to return to login");
         echo "<script> setTimeout(function() {
