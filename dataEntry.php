770e640c0b8f99b29c7070c7357ed54c2daf5750
Sam <sammvich@gmail.com>
2021-08-24 19:11:26 +1000

    / ‘dataEntry.php’

--- dataEntry.php
+++ dataEntry.php
@@ -37,7 +37,7 @@
             text-align: center;
             margin-left: auto;
             margin-right: auto;
-            margin-top: 15%;
+            margin-top: 0%;
         }
 
         tr.input {
