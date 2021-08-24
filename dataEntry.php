2944863dc660a70229b76bb664cd46cac872b1db
Sam <sammvich@gmail.com>
2021-08-24 19:12:12 +1000

    / ‘dataEntry.php’

--- dataEntry.php
+++ dataEntry.php
@@ -35,7 +35,7 @@
         table.input {
             width: 50%;
             text-align: center;
-            margin-left: 25%;
+            margin-left: 0%;
             margin-top: 0%;
         }
 
