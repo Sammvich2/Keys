4a3f8c2a87a501af2f765b8394688763f24662c3
Sam <sammvich@gmail.com>
2021-09-05 18:12:15 +1000

    / ‘index.php’

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-#ini_set('display_errors', 1);
-#ini_set('display_startup_errors', 1);
-#error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
     #print_r($_POST['user']);
     if ($_POST['login'] == "Login") {
         #print_r("Login started");
