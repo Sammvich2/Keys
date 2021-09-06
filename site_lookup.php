40ca08b1fc9bd11f9ad1eeca5c13588a06a02d13
Sam <sammvich@gmail.com>
2021-09-06 19:34:22 +1000

    / ‘site_lookup.php’

--- site_lookup.php
+++ site_lookup.php
@@ -60,6 +60,9 @@
 </h1>
 
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
     if(!isset($_COOKIE['sessionID'])) {
         print_r("Bye Bye nerd ;)");
