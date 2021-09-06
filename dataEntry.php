7fc1611f5680f06c63692d57f6e78b286f6f8cea
Sam <sammvich@gmail.com>
2021-09-06 17:32:36 +1000

    / ‘dataEntry.php’

--- dataEntry.php
+++ dataEntry.php
@@ -1,6 +1,8 @@
 <?php
-
 $pdo = new PDO('sqlite:keys.db');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 /*
 if(!isset($_COOKIE['sessionID'])) {
     print_r("Bye Bye Nerd ;)");
