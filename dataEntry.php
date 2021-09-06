c5b02ead84102d214c1ab9a3fdff9b8a49bcd47b
Sam <sammvich@gmail.com>
2021-09-06 18:44:01 +1000

    / ‘dataEntry.php’

--- dataEntry.php
+++ dataEntry.php
@@ -1,5 +1,6 @@
 <?php
 $pdo = new PDO('sqlite:keys.db');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
